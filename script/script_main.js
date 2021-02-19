c15f27894fab5600f8196f3cdb57be7c6bb22143
toby-code <mueller.to133@gmail.com>
2021-02-19 15:23:46 +0100

    Added description to existing functions

--- script/script_main.js
+++ script/script_main.js
@@ -1,11 +1,19 @@
+/**
+ * This function sets margin-right of the lineNav element to 8px and the width of lineNav to 6px.
+ * @param {number} lineNumber - This is the number of the navigation element.
+ */
 function navItemMove(lineNumber) {
-    lineNavigation = document.getElementById('lineNav' + lineNumber);
+    let lineNavigation = document.getElementById('lineNav' + lineNumber);
     lineNavigation.style.marginRight = '8px'; 
     lineNavigation.style.width = '6px';  
 }
 
+/**
+ * This function sets margin-right of the lineNav element to 32px and the width of lineNav to 0px.
+ * @param {number} lineNumber - This is the number of the navigation element.
+ */
 function navItemEnd(lineNumber) {
-    lineNavigation = document.getElementById('lineNav' + lineNumber);
+    let lineNavigation = document.getElementById('lineNav' + lineNumber);
     lineNavigation.style.marginRight = '32px';
     lineNavigation.style.width = '0px';    
 }
