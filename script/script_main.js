0a8e7cd3e8969b740d6b5ad343534d87cc2f93d3
Darsi1920 <78311987+Darsi1920@users.noreply.github.com>
2021-02-24 19:09:53 +0100

    Update script_main.js

--- script/script_main.js
+++ script/script_main.js
@@ -127,5 +127,5 @@ async function showBoardTasks() {
 }
 
 function generateBoardToDo(taskPosition) {
-    pushboardToDo.innerHTML = `<div>Hello ${tasks[taskPosition].category}</div>`
+    return`<div>Hello ${tasks[taskPosition].category}</div>`
 }
