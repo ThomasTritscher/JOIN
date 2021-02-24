5ebf7300207bf6943bad8cb016d11b094dc82b67
toby-code <mueller.to133@gmail.com>
2021-02-24 11:40:00 +0100

    Added javascript to add task

--- script/script_main.js
+++ script/script_main.js
@@ -1,3 +1,10 @@
+let tasks = [];
+let taskTitle;
+let taskDate;
+let taskCategory;
+let taskUrgency;
+let taskDescription;
+
 setURL('http://server-58.developerakademie.com/JOIN/backend');
 
 /**
@@ -21,10 +28,58 @@ function navItemEnd(lineNumber) {
 }
 
 /**
- * Downloads data from server.
+ * This function downloads data from server.
  */
 async function init() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
     userDatabase = JSON.parse(backend.getItem('userDatabase')) || [];
+}
+
+/**
+ * This function gets the title from the input and saves it in taskTitle.
+ */
+function getTitle() {
+    let titleInput = document.getElementById('title-input');
+    taskTitle = titleInput.value;
+}
+
+/**
+ * This function gets the date from the input and saves it in taskDate.
+ */
+function getDate() {
+    let dateInput = document.getElementById('date-input');
+    taskDate = dateInput.value; 
+}
+
+/**
+ * This function gets the category from the input and saves it in taskCategory.
+ */
+function getCategory() {
+    let categoryInput = document.getElementById('category-input');
+    taskCategory = categoryInput.value; 
+}
+
+/**
+ * This function gets the urgency from the input and saves it in taskUrgency.
+ */
+function getUrgency() {
+    let urgencyInput = document.getElementById('urgency-input');
+    taskUrgency = urgencyInput.value; 
+}
+
+/**
+ * This function gets the description from the input and saves it in taskDescription.
+ */
+function getDescription() {
+    let descriptionInput = document.getElementById('description-input');
+    taskDescription = descriptionInput.value; 
+}
+
+function cancelTask() {
+    alert('Clear input fields!');
+}
+
+function addTask() {
+    alert('Save task to server');
 }
